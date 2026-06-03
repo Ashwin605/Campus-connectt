d4d94f69f392861d5d5b542ed28cc5165bb90c86
Ashwin605 <ashwinsrichandra2008@gmail.com>
2026-06-03 20:36:29 +0530

    Fix useRef TypeScript error in NetworkGraph

--- src/components/3d/NetworkGraph.tsx
+++ src/components/3d/NetworkGraph.tsx
@@ -6,7 +6,7 @@ import { Points, PointMaterial } from "@react-three/drei";
 import * as THREE from "three";
 
 function NetworkNodes(props: any) {
-  const ref = useRef<any>();
+  const ref = useRef<any>(null);
   
   const sphere = useMemo(() => {
     // Generate random points in a sphere
